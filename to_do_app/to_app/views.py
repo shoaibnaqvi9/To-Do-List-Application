032209d7671ab1ca311941bab99c35c83a910e26
Syed Muhammad Shoaib Naqvi <muhshoaib@yahoo.com>
2024-09-30 12:30:36 +0500

    "Updated templates, views, and models for to-do app, and modified HTML and CSS."

--- to_do_app/to_app/views.py
+++ to_do_app/to_app/views.py
@@ -2,16 +2,19 @@ from django.shortcuts import render,redirect
 import re
 from django.contrib import messages
 from .models import User, To_do_List
+from django.db import IntegrityError
 
 def index(request):
-    return render(request, 'index.html')
+    if 'id' in request.session:
+        name= request.session.get('name')
+        return render(request, 'index.html', {'name': name})
 
 def signup(request):
     if request.method == 'POST':
-        username = request.POST.get['username']
-        email = request.POST.get['email']
-        password = request.POST.get['password']
-        confirm_password = request.POST.get['confirm_password']
+        name = request.POST.get('name')
+        email = request.POST.get('email')
+        password = request.POST.get('password')
+        confirm_password = request.POST.get('confirm_password')
         password_regex = re.compile(r'^(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$')
         if not password_regex.match(password):
             messages.error(request, "Password must be at least 8 characters long, include 1 uppercase letter, 1 number, and 1 special character.")
@@ -19,29 +22,37 @@ def signup(request):
         if password != confirm_password:
             messages.error(request, "Passwords do not match.")
             return render(request, "signup_here.html")
-        user = User(username=username, email=email, password=password)
-        user.save()
-        return redirect('login_here')
+        if not name or not email or not password:
+            messages.error(request, "All fields are required.")
+            print(name,email,password,confirm_password)
+        try:
+            user = User(name=name, email=email, password=password)
+            user.save()
+            return redirect('login_here')
+        except IntegrityError:
+            messages.error(request, "This email is already registered.")
+            return render(request, "signup_here.html")
+    return render(request, 'signup_here.html')
 
 def login(request):
     if request.method == 'POST':
-        email = request.POST.get['email']
-        password = request.POST.get['password']
+        email = request.POST.get('email')
+        password = request.POST.get('password')
         user = User.objects.filter(email=email)
         if user is not None:
             user = user[0]
             if user.password == password:
-                request.session['user_id'] = user.id
-                request.session['name'] = user.name
+                request.session['id'] = user.id
+                request.session['username'] = user.name
                 request.session['email'] = user.email
-                return redirect('dashboard')
+                return redirect('index')
             else:
                 msg = 'Invalid Credentials'
                 messages.error(request, msg)
-                return render(request, 'login_here.html', {'msg': msg})
-    else:
-        return render(request, 'login_here.html')
+        else:
+            messages.error(request, 'User does not exist')
+    return render(request, 'login_here.html')
 
 def logout(request):
-    logout(request)
-    return redirect('login')
+    request.session.clear()
+    return redirect('login_here')
